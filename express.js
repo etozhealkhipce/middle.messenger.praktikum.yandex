f78caa7006368b9e3a08031aca4ce5bf8ede3bb1
alkhipce <alkhipce@mail.ru>
2021-06-05 13:46:47 +0600

    fix: edit empty chat style

--- express.js
+++ express.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use("/", express.static(`${__dirname}/dist`));
+app.use("/", express.static(path.join(__dirname, "dist")));
 
 const PORT = 3000;
 
